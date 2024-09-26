bc38d136dd6f426e2190ebba0a28aaa9ca528eae
Auberson Kevin <114987481+kevinAuberson@users.noreply.github.com>
2024-09-26 17:55:16 +0200

    Add files via upload

---
+++ Labo-1/java/demo-app/src/main/java/ch/heig/dai/lab/tools/Main.java
@@ -0,0 +1,13 @@
+package ch.heig.dai.lab.tools;
+
+public class Main {
+    public static void main(String[] args) {
+
+        do {
+            System.out.println("Hello world!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) { }
+        }while (true);
+    }
+}
